1add0c061a3469a94270358ca5766ce1faaa90fc
Diego <diego@grayshirts.com.ar>
2020-01-30 22:34:48 -0300

    adelantos diagonal

--- src/main/java/com/meli/mutants/controllers/MainController.java
+++ src/main/java/com/meli/mutants/controllers/MainController.java
@@ -19,7 +19,7 @@ public class MainController {
 	
 	@RequestMapping(value = "", method= RequestMethod.POST)
 	public int checkMutant(@RequestBody DnaDto dnaSequence) {
-		int result = checkVerticalHits(dnaSequence.dna);
+		int result = checkDiagonalHits(dnaSequence.dna);
 		return result;
 	}
 	
@@ -32,9 +32,6 @@ public class MainController {
 				if (sequence[i].charAt(j) == sequence[i].charAt(j+1) ) {
 					int positionI = i;
 					int positionJ = j+1;
-					System.out.println("Actual hHits: "+hHits);
-					System.out.println("encontre letra "+sequence[i].charAt(j+1));
-					System.out.println("en: "+positionI+","+positionJ);
 					hHits++;
 					if (hHits>=3) {//cuando encuentre 3 pares sucesivos iguales o mas, es porque hay 4 letras iguales o 5 o mas
 						horizontalHits++;
@@ -47,40 +44,69 @@ public class MainController {
 	
 	private int checkVerticalHits(String[] sequence) {
 		
-		Map<Integer,Integer> columnVerticalCounters = Maps.newHashMap();
 		Map<Integer,Integer> temporaryVerticalCounters = Maps.newHashMap();
 		
 		int verticalHits = 0;
 		for (int i = 0; i < sequence.length-1; i++) {
 			for (int j = 0; j < sequence.length; j++) {
-				System.out.println("++++");
-				System.out.println("Evaluando: "+sequence[i].charAt(j));
-				System.out.println("Contra: "+sequence[i+1].charAt(j));
-				System.out.println("++++");
-				
 				if (sequence[i].charAt(j) == sequence[i+1].charAt(j) ) {
-					int positionI = i;
-					int positionJ = j;
-					System.out.println("====");
-					System.out.println("encontre letra "+sequence[i].charAt(j));
-					System.out.println("en: "+positionI+","+positionJ);
-			        
 					if (temporaryVerticalCounters.containsKey(j)) {
 			        	temporaryVerticalCounters.put(j, temporaryVerticalCounters.get(j)+1);
-			        	System.out.println("Actual hHits en columna "+j+": "+temporaryVerticalCounters.get(j));
 			        } else {
-			        	System.out.println("Actual hHits en columna "+j+": "+1);
 			        	temporaryVerticalCounters.put(j, 1);
 			        }
 					
 					if (temporaryVerticalCounters.containsKey(j) && temporaryVerticalCounters.get(j)>=3) {
 						verticalHits++;
 					}
-					
-					System.out.println("====");
 				}
 				else {
 					temporaryVerticalCounters.put(j, 0);
+				}
+			}
+		}
+		
+		return verticalHits;
+	}
+	
+	
+	private int checkDiagonalHits(String[] sequence) {
+		
+		Map<String,Integer> temporaryVerticalCounters = Maps.newHashMap();
+		
+		int verticalHits = 0;
+		String clave ="";
+		for (int i = 0; i < sequence.length-1; i++) {
+			
+			for (int j = 0; j < sequence.length-1; j++) {
+				
+				if (i==0 || j==0) {
+					clave = ""+i+j;
+				}
+				
+				System.out.println("++++");
+				System.out.println("Evaluando: "+sequence[i].charAt(j));
+				System.out.println("Contra: "+sequence[i+1].charAt(j+1));
+				System.out.println("++++");
+				
+				if (sequence[i].charAt(j) == sequence[i+1].charAt(j+1) ) {
+					System.out.println("====");
+					System.out.println("encontre letra "+sequence[i].charAt(j));
+					
+					if (temporaryVerticalCounters.containsKey(clave)) {
+			        	temporaryVerticalCounters.put(clave, temporaryVerticalCounters.get(clave)+1);
+			        } else {
+			        	temporaryVerticalCounters.put(clave, 1);
+			        }
+					System.out.println("Actual hHits en columna "+clave+": "+temporaryVerticalCounters.get(clave));
+					
+					if (temporaryVerticalCounters.containsKey(clave) && temporaryVerticalCounters.get(clave)>=3) {
+						verticalHits++;
+					}
+					System.out.println("====");
+				}
+				else {
+					temporaryVerticalCounters.put(clave, 0);
 				}
 			}
 		}
