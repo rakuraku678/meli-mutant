12e1b60da2d097aada5dc490905d0f45b0aa4f70
Diego <diego@grayshirts.com.ar>
2020-02-03 11:18:47 -0300

    detalle en stats

--- src/main/java/com/meli/mutants/dtos/StatsDto.java
+++ src/main/java/com/meli/mutants/dtos/StatsDto.java
@@ -3,6 +3,7 @@ package com.meli.mutants.dtos;
 import com.meli.mutants.models.Stats;
 
 public class StatsDto {
+
 	public int count_mutant_dna;
 	public int count_human_dna;
 	public float ratio;
@@ -10,14 +11,16 @@ public class StatsDto {
 	public StatsDto(Stats stats) {
 		this.count_human_dna = stats.getHumanCount();
 		this.count_mutant_dna = stats.getMutantCount();
-		if (this.count_human_dna == 0)
+		if (this.count_mutant_dna == 0) {
+			this.ratio = 0;
+		} else if (this.count_human_dna == 0)
 			this.ratio = 1;
 		else
 			this.ratio = (float) this.count_mutant_dna / this.count_human_dna;
 	}
-	
+
 	public StatsDto() {
 
 	}
-	
+
 }
